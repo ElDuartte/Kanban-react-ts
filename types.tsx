dc4159f88dc2253afb0def033fde9df8329d0c2d
juan felipe <jf@duartte.co>
2024-09-22 19:07:26 +0200

    Type definition and simple task dummy text

---
+++ types.tsx
@@ -0,0 +1,11 @@
+export interface TaskType {
+  id: number;
+  creator: string;
+  assigned: string;
+  title: string;
+  project: string;
+  description: string;
+  status: string;
+  date: string;
+  priority: string;
+}
