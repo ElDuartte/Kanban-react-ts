d989ab15b6c277c28738f76a76f7dfc11eea0616
juan felipe <jf@duartte.co>
2024-09-23 18:04:58 +0200

    Improving task list, adding more logic to generate columns

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -4,12 +4,27 @@ import Task from "../components/Task";
 import { TaskType } from "../../types";
 
 const TaskList: React.FC = () => {
+  const statuses = ["To Do", "In Progress", "Review"];
+
   return (
-    <div>
+    <div className="taskList-container">
       <h1>Task List</h1>
-      {tasksData.tasks.map((task: TaskType) => (
-        <Task key={task.id} task={task} />
-      ))}
+      <div className="status-container">
+        {statuses.map((status) => (
+          <div className="status-column-wrapper" key={status}>
+            <div className="status-header">
+              <h2 className="list-title">{status}</h2>
+            </div>
+            <div className="status-column">
+              {tasksData.tasks
+                .filter((task) => task.status === status)
+                .map((task: TaskType) => (
+                  <Task key={task.id} task={task} />
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
