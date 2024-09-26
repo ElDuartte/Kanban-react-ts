c6c9de208b5e5378d04796dbc58ef6f0415b1749
juan felipe <jf@duartte.co>
2024-09-26 17:22:44 +0200

    adding react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
+import CreateTask from "./pages/CreateTask.tsx";
+import TaskList from "./pages/TaskList.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TaskList />,
+  },
+  {
+    path: "/new",
+    element: <CreateTask />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
