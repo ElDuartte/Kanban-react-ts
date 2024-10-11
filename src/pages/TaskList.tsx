17a8f9250b70a4ec121d12b50784752a8c8e4c2d
juan felipe <jf@duartte.co>
2024-10-11 19:23:53 +0200

    Updating logic to the taskLIst

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,23 +1,40 @@
-import React from "react";
-import tasksData from "../../data/tasks.json";
+import React, { useEffect, useState } from "react";
 import Task from "../components/Task";
 import { TaskType } from "../../types";
+import { getTasksFromLocalStorage } from "../../localStorageUtils";
 
 const TaskList: React.FC = () => {
-  const statuses = ["To Do", "In Progress", "Review"];
+  const statuses = {
+    ToDo: "To Do",
+    InProgress: "In Progress",
+    Review: "Review",
+  };
+
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const storedTasks = getTasksFromLocalStorage();
+    setTasks(storedTasks);
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading tasks....</div>;
+  }
 
   return (
     <div className="taskList-container">
       <h1>Task List</h1>
       <div className="status-container">
-        {statuses.map((status) => (
+        {Object.values(statuses).map((status, index) => (
           <div className="status-column-wrapper" key={status}>
             <div className="status-header">
               <h2 className="list-title">{status}</h2>
             </div>
             <div className="status-column">
-              {tasksData.tasks
-                .filter((task) => task.status === status)
+              {tasks
+                .filter((task) => task.status === Object.keys(statuses)[index])
                 .map((task: TaskType) => (
                   <Task key={task.id} task={task} />
                 ))}
